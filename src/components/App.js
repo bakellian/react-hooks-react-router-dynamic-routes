0d371977587b693be210c0271f823e0e99ee376f
Bridget Akellian <akellianbv@gmail.com>
2022-05-16 13:03:29 -0400

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import MoviesPage from "./MoviesPage";
 
+// In the top-level component (App.js in this case), create our "parent" routes and render <MoviesPage>
+// In MoviesPage.js, render <MoviesList>
+// In MoviesList.js, iterate through the movies object and create a dynamic Link for each movie using its id
+// Back in MoviesPage.js, import useRouteMatch and create the child route by combining the current url with the :movie_id parameter; inside the child route, render <MovieShow>, passing the movies object as props
+// In MovieShow.js, import useParams; use the :movie_id from the params object to access the correct movie from the movies object and display it on the page
+
 function App() {
   const [movies, setMovies] = useState({
     1: { id: 1, title: "A River Runs Through It" },
